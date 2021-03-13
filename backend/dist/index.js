c912a4944d4159d84d532d516ef5e9b4e0de8366
JuanPabloRiveraAlvarezINEM <juanpariveraalvarez@gmail.com>
2021-03-13 09:32:44 -0500

    backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -11,7 +11,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const usuarios_1 = require("./routes/usuarios");
 const app = express_1.default();
 // settings
-app.set('port', 3000);
+app.set('port', 4000);
 // middlewares
 app.use(morgan_1.default('dev'));
 app.use(cors_1.default());
--- backend/src/routes/usuarios.ts
+++ backend/src/routes/usuarios.ts
@@ -1,15 +1,21 @@
-import {Router,Request,Response} from 'express'
-const usuarios = Router()
+import {Request,Response} from 'express'
+import Usuarios from '../models/usuarios.schema'
 
-export const registrar = (req:Request, res:Response)=>{
-  if(req.body.email || req.body.contrasena || req.body.nombre){
-    res.send('ingresado')
+export const registrar = async(req:Request, res:Response)=>{
+  console.log(req.body)
+  if(req.body.correo_usuario && req.body.contrasena_usuario && req.body.nombre_usuario){
+    const usuario = new Usuarios(req.body)
+    await usuario.save()
+    res.send('registrado')
   }else{
     res.send('no')
   }
 }
 
-export const ingresar = (req:Request, res:Response)=>{
-  res.send('mundo')
+export const ingresar = async(req:Request, res:Response):Promise<void> =>{
+  if(req.body.correo_usuario && req.body.contrasena_usuario && req.body.nombre_usuario){
+    const resulset = await Usuarios.find({"correo_usuario":req.body.correo_usuario})
+    res.send(resulset)
+  }
 }
 
